9920a945ad010e3c13375fc796379904ae7de152
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-03-17 13:08:57 -0300

    Add files via upload

---
+++ TP 1.cs
@@ -0,0 +1,23 @@
+﻿int A1 = 12 + 8 * 3;
+Console.WriteLine(A1);
+
+int A2 = (12 + 8) * 3;
+Console.WriteLine(A2);
+
+int A3 = 50 / 5 + 4;
+Console.WriteLine(A3);
+
+int A4 = 100 - 10 * 2;
+Console.WriteLine(A4);
+
+int A5 = 15 % 2;
+Console.WriteLine(A5);
+
+int A6 = 20 % 5;
+Console.WriteLine(A6);
+
+int A8 = 10 / 2 * 4;
+Console.WriteLine(A8);
+
+int A10 = 5 + 5 * 10 - 2;
+Console.WriteLine(A10);
