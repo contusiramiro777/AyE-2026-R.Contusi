56c161bc4b4d1c7f4b73cc198357fecc06bc045b
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-03-19 11:48:08 -0300

    Add files via upload

---
+++ TP 2 (Incompleto).cs
@@ -0,0 +1,124 @@
+﻿int a = 25;
+int b = 30;
+
+if (a > b)
+{
+ Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+int c = 14;
+int d = 14;
+
+if (c <= d)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+int e = 0;
+int f = 0;
+
+if (e == f)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+
+int g = 10;
+int h = 10;
+
+if (g != h)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+
+int i = 8;
+int j = 3;
+
+if (i >= j)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+
+int k = 15;
+int l = 15;
+
+if (k < l)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+
+int m = 100;
+int n = 50;
+
+if (m != n)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+
+int ñ = 7;
+float o = 7.0f;
+
+if (ñ == o)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+float p = 0.5f;
+int q = 0;
+
+if (p > q)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
+
+int r = -5;
+int s = -2;
+
+if (r < s)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("Falso");
+}
