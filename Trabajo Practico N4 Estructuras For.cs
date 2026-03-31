87ceaf11e81023458c571803e937b8b3f1713572
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-03-31 12:40:27 -0300

    Add files via upload

---
+++ Trabajo Practico N4 Estructuras For.cs
@@ -0,0 +1,36 @@
+﻿//Ejcercicio1
+for (int x=0; x<=10; x++)
+{
+    Console.WriteLine(x);
+}
+
+//Ejercicio2
+Console.WriteLine("");
+for (int a = 1; a <= 5; a++)
+{
+    Console.WriteLine("Hola, Mundo!");
+}
+
+//Ejercicio3
+Console.WriteLine("");
+for (int b= 2; b <= 20; b+=2)
+{
+    Console.WriteLine(b);
+}
+
+//Ejercicio4
+Console.WriteLine("");
+for (int c = 7; c <=70; c+=7)
+{
+    Console.WriteLine(c);
+}
+
+//Ejercicio5
+Console.WriteLine("");
+int acum;
+for (int d =1; d <= 5; d++)
+{
+    acum = d;
+    acum += d;
+    Console.WriteLine(acum);
+}
