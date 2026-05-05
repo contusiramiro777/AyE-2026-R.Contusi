034df3975189988d410d0c2157f0b023237d9129
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-05-05 11:03:25 -0300

    Add files via upload

---
+++ TP 7.cs
@@ -0,0 +1,75 @@
+﻿// Ejercicio 1
+
+Console.WriteLine("Ingrese un numero");
+bool esPrimo = true;
+int numero = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i < numero; i++)
+{
+    if (numero % i == 0)
+    {
+        esPrimo = false;
+        break;
+    }
+}
+if (esPrimo)
+{
+    Console.WriteLine("El numero es primo");
+}
+else
+{
+    Console.WriteLine("El numero no es primo");
+}
+
+// Ejercicio 2
+
+Console.WriteLine("");
+Console.WriteLine("Ingrese un numero no negativo");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 1; i <= numero2; i++)
+{
+    factorial *= i;
+}
+Console.WriteLine("El factorial de " + numero2 + " es " + factorial);
+
+//Ejercicio 3
+
+Console.WriteLine("");
+Console.WriteLine("Ingrese un numero");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+int a = 0, b = 1, c = 0;
+for (int i = 0; i < numero3; i++)
+{
+    Console.Write(a + " ");
+    c = a + b;
+    a = b;
+    b = c;
+}
+//Ejercicio 4
+
+Console.WriteLine("");
+Console.WriteLine("Ingrese una accion:");
+Console.WriteLine("1. Saludar");
+Console.WriteLine("2. Despedirse");
+Console.WriteLine("3. Salir");
+bool salir = false;
+while (salir == false)
+{
+    int opcion = Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("¡Hola! ¿Cómo estás?");
+            break;
+        case 2:
+            Console.WriteLine("¡Adiós! Que tengas un buen día.");
+            break;
+        case 3:
+            Console.WriteLine("Saliendo del programa...");
+            salir = true;
+            break;
+        default:
+            Console.WriteLine("Opción no válida. Por favor, seleccione una opción del 1 al 3.");
+            break;
+    }
+}
