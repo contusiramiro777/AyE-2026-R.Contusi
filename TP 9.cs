a5baab4f467f269af94e546b638cedd0a19ef50b
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-05-05 11:17:17 -0300

    Add files via upload

---
+++ TP 9.cs
@@ -0,0 +1,179 @@
+﻿//Ejercicio1
+
+try
+{
+    string Ejercicio1(int N)
+    {
+        string resultado;
+        if (N > 0)
+        {
+            resultado = "El número es positivo.";
+            return resultado;
+        }
+        else if (N < 0)
+        {
+            resultado = "El número es negativo";
+            return resultado;
+        }
+        else
+        {
+            resultado = "El número es cero";
+            return resultado;
+        }
+    }
+
+    Console.WriteLine("¿Numero positivo?");
+
+    Console.WriteLine(Ejercicio1(Convert.ToInt32(Console.ReadLine())));
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("ERROR: Alcanzaste el limite.");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+
+
+//Ejercicio 2
+try
+{
+    string Ejercicio2(int Q)
+    {
+        string resultado;
+        if (Q >= 18)
+        {
+            resultado = "¡Bienvenido a la fiesta!.";
+            return resultado;
+        }
+        else
+        {
+            resultado = "Lo siento, eres muy joven";
+            return resultado;
+        }
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Ejercicio 2");
+    Console.WriteLine("¿Mayor de edad?");
+    Console.WriteLine("Ingrese su edad");
+    Console.WriteLine(Ejercicio2(Convert.ToInt32(Console.ReadLine())));
+
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("ERROR: Alcanzaste el limite.");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+
+
+try
+{
+    string Ejercicio3(string contraseña)
+    {
+        string resultado;
+        if (contraseña == "python123")
+        {
+            resultado = "¡Contraseña correcta! Acceso concedido.";
+            return resultado;
+        }
+        else
+        {
+            resultado = "¡Contraseña incorrecta, Autodestrucción en 5 minutos!";
+            return resultado;
+        }
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Ejercicio 3");
+    Console.WriteLine("Contraseña secreta");
+    Console.WriteLine("Ingrese la contraseña");
+    Console.WriteLine(Ejercicio3(Console.ReadLine()));
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+
+try
+{
+    string Ejercicio4(int numero)
+    {
+        string resultado;
+        if (numero % 2 == 0)
+        {
+            resultado = "El número es par.";
+            return resultado;
+        }
+        else
+        {
+            resultado = "El número es impar.";
+            return resultado;
+        }
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Ejercicio 4");
+    Console.WriteLine("¿Par o Impar?");
+    Console.WriteLine("Ingrese un número");
+    Console.WriteLine(Ejercicio4(Convert.ToInt32(Console.ReadLine())));
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("ERROR: Alcanzaste el limite.");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+
+try
+{
+    string Ejercicio5(int edad, string compro)
+    {
+        string resultado;
+        if (edad >= 65 && compro == "si")
+        {
+            resultado = "¡Felicidades! Tienes entrada gratuita al cine";
+            return resultado;
+        }
+        else
+        {
+            resultado = "Compre la entrada o retirese del local";
+            return resultado;
+        }
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Ejercicio 5");
+    Console.WriteLine("¿Entrada gratis?");
+    Console.WriteLine("Ingrese su edad");
+    int edad = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ha comprado palomitas de maíz?");
+    string compro = Console.ReadLine();
+    Console.WriteLine(Ejercicio5(edad, compro));
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("ERROR: Alcanzaste el limite.");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("ERROR: Acabaste de ingresar un caracter no valido del int.");
+}
