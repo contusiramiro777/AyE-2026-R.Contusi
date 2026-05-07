084dd7a8972e6bd4200256363bb69b9e4a184b81
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-05-07 08:56:38 -0300

    Update Ahorcado.cs

--- Ahorcado.cs
+++ Ahorcado.cs
@@ -1,52 +1,54 @@
-﻿try(Console.WriteLine("Juguemos al ahorcado");
-string[] palabras = { "programacion", "algoritmos", "advincula", "computadora", "balatro" };
-Random random = new Random();
-for (int i = 0; i < palabras.Length; i++)
-{
-    int indice = random.Next(palabras.Length);
-    string palabraSecreta = palabras[indice];
-    char[] palabraAdivinada = new char[palabraSecreta.Length];
-    for (int j = 0; j < palabraAdivinada.Length; j++)
+try {
+    Console.WriteLine("Juguemos al ahorcado");
+    string[] palabras = { "programacion", "algoritmos", "advincula", "computadora", "minecraft" };
+    Random random = new Random();
+    for (int i = 0; i < palabras.Length; i++)
     {
-        palabraAdivinada[j] = '_';
-    }
-    int intentosRestantes = 7;
-    bool juegoTerminado = false;
-    while (!juegoTerminado)
-    {
-        Console.WriteLine("Palabra: " + new string(palabraAdivinada));
-        Console.WriteLine("Intentos restantes:" + intentosRestantes);
-        Console.Write("Ingresa una letra: ");
-        char letra = Console.ReadLine()[0];
-        if (palabraSecreta.Contains(letra))
+        int indice = random.Next(palabras.Length);
+        string palabraSecreta = palabras[indice];
+        char[] palabraAdivinada = new char[palabraSecreta.Length];
+        for (int j = 0; j < palabraAdivinada.Length; j++)
         {
-            for (int k = 0; k < palabraSecreta.Length; k++)
+            palabraAdivinada[j] = '_';
+        }
+        int intentosRestantes = 7;
+        bool juegoTerminado = false;
+        while (!juegoTerminado)
+        {
+            Console.WriteLine("Palabra: " + new string(palabraAdivinada));
+            Console.WriteLine("Intentos restantes:" + intentosRestantes);
+            Console.Write("Ingresa una letra: ");
+            char letra = Console.ReadLine()[0];
+            if (palabraSecreta.Contains(letra))
             {
-                if (palabraSecreta[k] == letra)
+                for (int k = 0; k < palabraSecreta.Length; k++)
                 {
-                    palabraAdivinada[k] = letra;
+                    if (palabraSecreta[k] == letra)
+                    {
+                        palabraAdivinada[k] = letra;
+                    }
                 }
             }
-        }
-        else
-        {
-            intentosRestantes--;
-        }
-        if (new string(palabraAdivinada) == palabraSecreta)
-        {
-            Console.WriteLine("¡Felicidades! Has adivinado la palabra: " + palabraSecreta);
-            juegoTerminado = true;
-            break;
-        }
-        else if (intentosRestantes == 0)
-        {
-            Console.WriteLine("Has perdido, La palabra era:" + palabraSecreta);
-            juegoTerminado = true;
-            break;
+            else
+            {
+                intentosRestantes--;
+            }
+            if (new string(palabraAdivinada) == palabraSecreta)
+            {
+                Console.WriteLine("¡Felicidades! Has adivinado la palabra: " + palabraSecreta);
+                juegoTerminado = true;
+                break;
+            }
+            else if (intentosRestantes == 0)
+            {
+                Console.WriteLine("Has perdido, La palabra era:" + palabraSecreta);
+                juegoTerminado = true;
+                break;
+            }
         }
     }
-})
+}
 catch (System.IndexOutOfRangeException)
 {
-       Console.WriteLine("Error: No se han definido palabras para el juego.");
+    Console.WriteLine("Error: No se han definido palabras para el juego.");
 }
