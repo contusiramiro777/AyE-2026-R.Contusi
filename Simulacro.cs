df60c90ab880a0e6f4d9367ba41b321245a03c31
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-06-16 12:22:50 -0300

    Add files via upload

---
+++ Simulacro.cs
@@ -0,0 +1,41 @@
+﻿//Ejercicio2
+
+int numero, potencia;
+
+Console.WriteLine("Ingrese un numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese la potencia a la que desea elevar el numero: ");
+potencia = Convert.ToInt32(Console.ReadLine());
+    
+Console.WriteLine($"El resultado de elevar {numero} a la potencia {potencia} es: {CalcularPotencia(numero, potencia)}");
+
+int CalcularPotencia(int baseNum, int exp)
+{
+    if (exp == 0)
+    {
+        return 1;
+    }
+    else if (exp < 0)
+    {
+        return 1 / CalcularPotencia(baseNum, -exp); 
+    }
+    else
+    {
+        return baseNum * CalcularPotencia(baseNum, exp - 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
