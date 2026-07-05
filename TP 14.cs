64f23c9a865d47432a08483df1ce1d12907bbe0d
Contusi Ramiro <ramirocontusi.et32@gmail.com>
2026-07-05 19:50:23 -0300

    TP 14

--- TP 14.cs
+++ TP 14.cs
@@ -1,5 +1,4 @@
-﻿﻿//Ejercicio 1
-Console.WriteLine("Ejercicio 1: Carga y Recorrido Básico");
+﻿﻿//Ejercicio1
 Console.WriteLine("Ingrese 5 números:");
 int[] numeros = new int[5];
 for (int i = 0; i < numeros.Length; i++)
@@ -14,8 +13,7 @@ foreach (int numero in numeros)
     Console.WriteLine(numero);
 }
 
-//Ejercicio 2
-Console.WriteLine("Ejercicio 2: Búsqueda y Conteo");
+//Ejercicio2
 string[] frutas = new[] { "manzana", "banana", "naranja", "uva", "pera" };
 Console.WriteLine("Ingrese el nombre de una fruta:");
 string frutaBuscada = Console.ReadLine();
@@ -29,8 +27,7 @@ else
     Console.WriteLine($"La fruta {frutaBuscada} no fue encontrada.");
 }
 
-//Ejercicio 3
-Console.WriteLine("Ejercicio 3: Suma y Promedio");
+//Ejercicio3
 double[] notas = new double[10];
 Console.WriteLine("Ingrese las notas de los estudiantes:");
 for (int i = 0; i < notas.Length; i++)
@@ -48,9 +45,7 @@ double promedio = suma / notas.Length;
 Console.WriteLine($"La suma total de las notas es: {suma}");
 Console.WriteLine($"El promedio de las notas es: {promedio}");
 
-//Ejercicio 4
-
-Console.WriteLine("Ejercicio 4: Encontrar el Valor Máximo y Mínimo");
+//Ejercicio4
 double[] temperaturas = new[] { 32.0, 28.0, 30.0, 35.0, 31.0, 29.0, 27.0 };
 double max = temperaturas[0];
 double min = temperaturas[0];
@@ -62,8 +57,7 @@ foreach (double temp in temperaturas)
 Console.WriteLine($"La temperatura máxima es: {max}");
 Console.WriteLine($"La temperatura mínima es: {min}");
 
-//Ejercicio 5
-Console.WriteLine("Ejercicio 5: Ordenamiento");
+//Ejercicio5
 int[] numerosDesordenados = new[] { 5, 2, 9, 1, 4, 6 };
 for (int i = 0; i < numerosDesordenados.Length; i++)
 {
@@ -83,8 +77,7 @@ foreach (int numero in numerosDesordenados)
     Console.WriteLine(numero);
 }
 
-//Ejercicio 6
-Console.WriteLine("Ejercicio 6: Pares e Impares");
+//Ejercicio6
 int[] numerosEnteros = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 int pares = 0;
 int impares = 0;
